9164afb7d3278db283de1c048c11bdff730f8be9
hussain-shariff <hussainowaiz070@gmail.com>
2023-04-20 15:54:28 +0530

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <NavBar/>
         <Routes>
-          
+          <Route index element={<Home/>}></Route>
         </Routes>
       </Router>
     </div>
